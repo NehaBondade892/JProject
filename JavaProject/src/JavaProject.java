0a1fcbc99e64d30b32d8db9e3e27daef95184157
Neha Bondade <c0819892@mylambton.ca>
2021-07-24 15:17:27 -0400

    NehaBondade : Added sample file.

---
+++ JavaProject/src/JavaProject.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Ashu
+ *
+ */
+public class JavaProject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Neha Bondade");
+
+	}
+
+}
